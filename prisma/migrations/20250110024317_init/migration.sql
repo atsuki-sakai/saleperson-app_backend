bf16259f07747db53a41d83b0ddc49411b1397f5
酒井専冴 <atsukisakai@sakaisensaenoMacBook-Pro-2.local>
2025-01-10 12:21:01 +0900

    fix: env variable changed

--- prisma/migrations/20250110024317_init/migration.sql
+++ prisma/migrations/20250110024317_init/migration.sql
@@ -1,19 +1,3 @@
-/*
-  Warnings:
-
-  - You are about to drop the `Document` table. If the table is not empty, all the data it contains will be lost.
-  - You are about to drop the `Store` table. If the table is not empty, all the data it contains will be lost.
-
-*/
--- DropForeignKey
-ALTER TABLE "Document" DROP CONSTRAINT "Document_store_id_fkey";
-
--- DropTable
-DROP TABLE "Document";
-
--- DropTable
-DROP TABLE "Store";
-
 -- CreateTable
 CREATE TABLE "store" (
     "id" TEXT NOT NULL,
